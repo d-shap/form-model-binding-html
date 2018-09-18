ddbdd843c146c709ce8e4ca0986bad3c313d3386
Dmitry Shapovalov <shenf@mail.ru>
2018-09-18 22:34:25 +0300

    Источник данных для связывания модели

---
+++ src/main/java/ru/d_shap/formmodel/binding/html/HtmlBindingSource.java
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model html binding is a form model implementation for HTML.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of Form model html binding.
+//
+// Form model html binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model html binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.html;
+
+import ru.d_shap.formmodel.binding.model.BindingSource;
+
+/**
+ * The HTML binding source.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class HtmlBindingSource implements BindingSource {
+
+    /**
+     * Create new object.
+     */
+    public HtmlBindingSource() {
+        super();
+    }
+
+}
