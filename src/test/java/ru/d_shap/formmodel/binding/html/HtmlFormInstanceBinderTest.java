d4d547dd79308169a76c7ab925ee6952f54b7ec5
Dmitry Shapovalov <shenf@mail.ru>
2018-10-29 16:05:09 +0300

    Добавил заглушки для тестов

---
+++ src/test/java/ru/d_shap/formmodel/binding/html/HtmlFormInstanceBinderTest.java
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model html binding is a form model binding implementation for HTML.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of Form model html binding.
+//
+// Form model html binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model html binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.html;
+
+/**
+ * Tests for {@link HtmlFormInstanceBinder}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class HtmlFormInstanceBinderTest {
+
+    /**
+     * Test class constructor.
+     */
+    public HtmlFormInstanceBinderTest() {
+        super();
+    }
+
+}
