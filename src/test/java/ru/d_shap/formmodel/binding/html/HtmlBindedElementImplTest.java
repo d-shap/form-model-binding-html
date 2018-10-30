442b720ba52178d6439834d342765c18f410119e
Dmitry Shapovalov <shenf@mail.ru>
2018-10-30 14:13:52 +0300

    Тесты для элемента

--- src/test/java/ru/d_shap/formmodel/binding/html/HtmlBindedElementImplTest.java
+++ src/test/java/ru/d_shap/formmodel/binding/html/HtmlBindedElementImplTest.java
@@ -19,6 +19,14 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.formmodel.binding.html;
 
+import java.util.List;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import ru.d_shap.assertions.Assertions;
+
 /**
  * Tests for {@link HtmlBindedElementImpl}.
  *
@@ -31,6 +39,138 @@ public final class HtmlBindedElementImplTest {
      */
     public HtmlBindedElementImplTest() {
         super();
+    }
+
+    /**
+     * {@link HtmlBindedElementImpl} class test.
+     */
+    @Test
+    public void getElementTest() {
+        String xml = "<?xml version='1.0'?>\n";
+        xml += "<ns1:form id='id' xmlns:ns1='http://d-shap.ru/schema/form-model/1.0'>";
+        xml += "<ns1:element id='pclass' lookup='p.pclass' type='optional+'>";
+        xml += "</ns1:element>";
+        xml += "<ns1:element id='blockclass' lookup='.blockclass' type='optional+'>";
+        xml += "</ns1:element>";
+        xml += "</ns1:form>";
+        HtmlFormBinder htmlFormBinder = TestHelper.createHtmlFormBinder(xml);
+        String html = createHtml();
+        Document document = htmlFormBinder.bindHtml(html, "id");
+
+        List<Element> elements1 = htmlFormBinder.getElementsWithId(document, "pclass");
+        List<HtmlBindedElement> bindedElements1 = htmlFormBinder.getBindedElements(elements1);
+        Assertions.assertThat(bindedElements1).hasSize(2);
+        Assertions.assertThat(bindedElements1.get(0).getElement()).isNotNull();
+        Assertions.assertThat(bindedElements1.get(0).getElement().tag().getName()).isEqualTo("p");
+        Assertions.assertThat(bindedElements1.get(1).getElement()).isNotNull();
+        Assertions.assertThat(bindedElements1.get(1).getElement().tag().getName()).isEqualTo("p");
+
+        List<Element> elements2 = htmlFormBinder.getElementsWithId(document, "blockclass");
+        List<HtmlBindedElement> bindedElements2 = htmlFormBinder.getBindedElements(elements2);
+        Assertions.assertThat(bindedElements2).hasSize(2);
+        Assertions.assertThat(bindedElements2.get(0).getElement()).isNotNull();
+        Assertions.assertThat(bindedElements2.get(0).getElement().tag().getName()).isEqualTo("div");
+        Assertions.assertThat(bindedElements2.get(1).getElement()).isNotNull();
+        Assertions.assertThat(bindedElements2.get(1).getElement().tag().getName()).isEqualTo("span");
+    }
+
+    /**
+     * {@link HtmlBindedElementImpl} class test.
+     */
+    @Test
+    public void getOwnTextTest() {
+        String xml = "<?xml version='1.0'?>\n";
+        xml += "<ns1:form id='id' xmlns:ns1='http://d-shap.ru/schema/form-model/1.0'>";
+        xml += "<ns1:element id='pclass' lookup='p.pclass' type='optional+'>";
+        xml += "</ns1:element>";
+        xml += "<ns1:element id='blockclass' lookup='.blockclass' type='optional+'>";
+        xml += "</ns1:element>";
+        xml += "</ns1:form>";
+        HtmlFormBinder htmlFormBinder = TestHelper.createHtmlFormBinder(xml);
+        String html = createHtml();
+        Document document = htmlFormBinder.bindHtml(html, "id");
+
+        List<Element> elements1 = htmlFormBinder.getElementsWithId(document, "pclass");
+        List<HtmlBindedElement> bindedElements1 = htmlFormBinder.getBindedElements(elements1);
+        Assertions.assertThat(bindedElements1).hasSize(2);
+        Assertions.assertThat(bindedElements1.get(0).getOwnText()).isEqualTo("This is a to somewhere.");
+        Assertions.assertThat(bindedElements1.get(1).getOwnText()).isEqualTo("");
+
+        List<Element> elements2 = htmlFormBinder.getElementsWithId(document, "blockclass");
+        List<HtmlBindedElement> bindedElements2 = htmlFormBinder.getBindedElements(elements2);
+        Assertions.assertThat(bindedElements2).hasSize(2);
+        Assertions.assertThat(bindedElements2.get(0).getOwnText()).isEqualTo("");
+        Assertions.assertThat(bindedElements2.get(1).getOwnText()).isEqualTo("Span in Div 2.");
+    }
+
+    /**
+     * {@link HtmlBindedElementImpl} class test.
+     */
+    @Test
+    public void getTextTest() {
+        String xml = "<?xml version='1.0'?>\n";
+        xml += "<ns1:form id='id' xmlns:ns1='http://d-shap.ru/schema/form-model/1.0'>";
+        xml += "<ns1:element id='pclass' lookup='p.pclass' type='optional+'>";
+        xml += "</ns1:element>";
+        xml += "<ns1:element id='blockclass' lookup='.blockclass' type='optional+'>";
+        xml += "</ns1:element>";
+        xml += "</ns1:form>";
+        HtmlFormBinder htmlFormBinder = TestHelper.createHtmlFormBinder(xml);
+        String html = createHtml();
+        Document document = htmlFormBinder.bindHtml(html, "id");
+
+        List<Element> elements1 = htmlFormBinder.getElementsWithId(document, "pclass");
+        List<HtmlBindedElement> bindedElements1 = htmlFormBinder.getBindedElements(elements1);
+        Assertions.assertThat(bindedElements1).hasSize(2);
+        Assertions.assertThat(bindedElements1.get(0).getText()).isEqualTo("This is a link to somewhere.");
+        Assertions.assertThat(bindedElements1.get(1).getText()).isEqualTo("");
+
+        List<Element> elements2 = htmlFormBinder.getElementsWithId(document, "blockclass");
+        List<HtmlBindedElement> bindedElements2 = htmlFormBinder.getBindedElements(elements2);
+        Assertions.assertThat(bindedElements2).hasSize(2);
+        Assertions.assertThat(bindedElements2.get(0).getText()).isEqualTo("Span in Div 1.");
+        Assertions.assertThat(bindedElements2.get(1).getText()).isEqualTo("Span in Div 2.");
+    }
+
+    /**
+     * {@link HtmlBindedElementImpl} class test.
+     */
+    @Test
+    public void getAttributeValueTest() {
+
+    }
+
+    /**
+     * {@link HtmlBindedElementImpl} class test.
+     */
+    @Test
+    public void getAbsoluteAttributeValueTest() {
+
+    }
+
+    private String createHtml() {
+        String html = "";
+        html += "<html>";
+        html += "<head>";
+        html += "<title>Test page</title>";
+        html += "</head>";
+        html += "<body>";
+        html += "<p class='pclass'>";
+        html += "This is a ";
+        html += "<a href='linkref'>link</a>";
+        html += " to somewhere.";
+        html += "</p>";
+        html += "<p class='pclass'>";
+        html += "<div class='blockclass'>";
+        html += "<span>Span in Div 1.</span>";
+        html += "</div>";
+        html += "<div>";
+        html += "<span class='blockclass'>Span in Div 2.</span>";
+        html += "</div>";
+        html += "</p>";
+        html += "</body>";
+        html += "</html>";
+        return html;
     }
 
 }
